37b068400676f06f542d410da2fa92d5a2b5357b
Enzoo_Kms <ekemmas@edenschool.fr>
2024-02-10 23:40:38 +0100

    Console App Information

---
+++ server/console.js
@@ -0,0 +1,14 @@
+require("colors");
+console.clear();
+
+console.log(
+  `/*
+|-------------------------------------------------------------------------------------------|
+| ${config.name.yellow} -- ${
+    config.version.green
+  } // Server run at current url : ${
+    `http://${config.url}:${config.port}`.blue
+  }              |
+|-------------------------------------------------------------------------------------------|
+*/`
+);
