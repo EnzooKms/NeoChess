022f1bc56fa5c5a1a608b1932e2ead6161289bff
Enzoo_Kms <ekemmas@edenschool.fr>
2024-02-10 23:39:27 +0100

    Project installer

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# install node package
+npm i bcrypt colors dotenv express express-session express-edge multer sqlite3 socket.io
+
+# clear console
+clear
+
+# run projet
+powershell & npm run server
