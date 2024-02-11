a6a336f8ab8ede4ed0d0ca44e13618fe703091f0
Enzoo_Kms <ekemmas@edenschool.fr>
2024-02-11 18:25:13 +0100

    ssl installer

--- install.sh
+++ install.sh
@@ -6,5 +6,13 @@ npm i bcrypt colors compressions cors dotenv express express-session express-edg
 # clear console
 clear
 
+# ssl
+mkdir ssl
+cd ./ssl
+openssl genrsa -out key.pem
+openssl req -new -key key.pem -out csr.pem
+openssl x509 -req -days 9999 -in csr.pem -signkey key.pem -out cert.pem
+cd ../
+
 # run projet
 powershell & npm run server
