c3a0ff9f265acf925a6a552d1d24ddb132159e17
Enzoo_Kms <ekemmas@edenschool.fr>
2024-02-11 20:22:58 +0100

    get auth/register

---
+++ server/routers/routes/auth/register.js
@@ -0,0 +1,20 @@
+/*************************************************************
+ *
+ * Auth/register router
+ *
+ *************************************************************/
+
+const Route = require("../../route.js");
+const route = new Route();
+
+const get = route.route("/:language/auth/register", (req, res) => {
+  const translate = req.load("auth/register");
+  const langName = req.params.language;
+  const asset = `https://${config.url}:${config.port}${config.path_public_assets}`;
+
+  if (translate) {
+    res.render("auth.register", { translate, langName, asset });
+  }
+});
+
+module.exports = { get };
