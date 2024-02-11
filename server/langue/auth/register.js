9aa124de1ce9e406e3070703d482c9b4eb300209
Enzoo_Kms <ekemmas@edenschool.fr>
2024-02-11 20:20:55 +0100

    translate the view auth/register

---
+++ server/langue/auth/register.js
@@ -0,0 +1,11 @@
+module.exports = {
+  fr: {
+    title: "Créer un compte",
+    username: "Veuillez mettre votre nom d'utilisateur",
+    password: "Veuillez mettre votre mot de passe",
+    welcome: "Bienvenue à NeoChess",
+    register:
+      "inscrivez-vous pour créer votre premier compte et commencer à jouer aux échecs dans NeoChess",
+    submit: "Créer votre compte",
+  },
+};
