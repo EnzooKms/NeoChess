ea9912df3a08cc3eb3d744a9672a58a49f60f873
Enzoo_Kms <ekemmas@edenschool.fr>
2024-02-16 17:43:03 +0100

    change path url

--- server/routers/routes/auth/register.js
+++ server/routers/routes/auth/register.js
@@ -7,7 +7,7 @@
 const Route = require("../../route.js");
 const route = new Route();
 
-const get = route.route("/:language?/auth/register", (req, res) => {
+const get = route.route("/:language?/register", (req, res) => {
   const translate = req.load("auth/register");
   const langName = req.params.language;
 
