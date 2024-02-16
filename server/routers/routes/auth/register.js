bef55e517003f6cbd983f7cd8be6a6b67e5ec683
Enzoo_Kms <ekemmas@edenschool.fr>
2024-02-16 17:35:00 +0100

    use helper

--- server/routers/routes/auth/register.js
+++ server/routers/routes/auth/register.js
@@ -10,10 +10,9 @@ const route = new Route();
 const get = route.route("/:language?/auth/register", (req, res) => {
   const translate = req.load("auth/register");
   const langName = req.params.language;
-  const asset = `https://${config.url}:${config.port}${config.path_public_assets}`;
 
   if (translate) {
-    res.render("auth.register", { translate, langName, asset });
+    res.render("auth/register", { translate, langName, h: req.helper });
   }
 });
 
