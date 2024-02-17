1b4d1af7f97cbf9fd95c33ba7fd061d2677f345d
Enzoo_Kms <ekemmas@edenschool.fr>
2024-02-17 16:17:32 +0100

    add old if client make bad req

--- server/helper.js
+++ server/helper.js
@@ -2,6 +2,12 @@ function asset(asset) {
   return `${config.path_public_assets}/${asset}`;
 }
 
+function old(old, property) {
+  if (!old) return "";
+  return old[property] ?? "";
+}
+
 module.exports = {
   asset,
+  old,
 };
