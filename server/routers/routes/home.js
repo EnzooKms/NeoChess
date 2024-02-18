8c3094393b8e94ebd8aa3abdc74adc04b67e0b36
Enzoo_Kms <ekemmas@edenschool.fr>
2024-02-19 00:05:20 +0100

    render home

--- server/routers/routes/home.js
+++ server/routers/routes/home.js
@@ -8,7 +8,12 @@ const Route = require("../route.js");
 const route = new Route();
 
 const get = route.route("/", (req, res) => {
-  res.render("home", { title: "Home" });
+  const translate = req.load("home");
+  const langName = req.params.language || config.default_language;
+
+  if (translate) {
+    res.render("home", { translate, langName, h: req.helper });
+  }
 });
 
 module.exports = { get };
