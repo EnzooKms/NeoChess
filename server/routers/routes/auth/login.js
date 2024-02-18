f38b313aa8abaab151bbc55241207316b10287a5
Enzoo_Kms <ekemmas@edenschool.fr>
2024-02-19 00:04:51 +0100

    finish backend

--- server/routers/routes/auth/login.js
+++ server/routers/routes/auth/login.js
@@ -12,8 +12,36 @@ const get = route.route("/:language?/login", (req, res) => {
   const langName = req.params.language;
 
   if (translate) {
-    res.render("auth/login", { translate, langName, h: req.helper });
+    res.render("auth/login", { translate, langName, h: req.helper, old: req.session.old, errors: req.session.errors });
   }
 });
 
-module.exports = { get };
+const post = route.route("/:lanuage?/login", async (req, res) => {
+  const { username, password } = req.body;
+  const langName = req.params.language || config.default_language;
+  const { sequelize } = require("../../../service/database/models/");
+  const User = sequelize.models.User;
+  const user = await User.findOne({
+    where: {
+      username,
+      password,
+    },
+  });
+
+  if (user) {
+    console.log(user.dataValues);
+    req.session.user = {
+      username: user.dataValues.username,
+      createdAt: user.dataValues.createdAt,
+    };
+    res.redirect("/");
+  } else {
+    const error = "notExist";
+    const msgError = require("../../../langue/errors.js")[langName].ORM[error];
+    req.session.errors = { msgError };
+    req.session.old = { username, password };
+    res.redirect(`/${langName}/login`);
+  }
+});
+
+module.exports = { get, post };
--- server/routers/routes/auth/register.js
+++ server/routers/routes/auth/register.js
@@ -6,8 +6,17 @@
 
 const Route = require("../../route.js");
 const route = new Route();
+const validator = require("../../../service/validator.js");
 
 const get = route.route("/:language?/register", (req, res) => {
+  /*************************************************************
+   *
+   * Init session and translation
+   *
+   *************************************************************/
+
+  const defaultSession = { username: [], password: [] };
+  if (!req.session.errors) req.session.errors = defaultSession;
   const translate = req.load("auth/register");
   const langName = req.params.language || config.default_language;
 
@@ -17,19 +26,57 @@ const get = route.route("/:language?/register", (req, res) => {
 });
 
 const post = route.route("/:language?/register", async (req, res) => {
+  /*************************************************************
+   *
+   * Init some variable
+   *
+   *************************************************************/
+
   const { sequelize } = require("../../../service/database/models/");
   const User = sequelize.models.User;
   const { username, password } = req.body;
+  console.log(req.body);
   const langName = req.params.language || config.default_language;
 
-  await User.create({ username, password }).catch((err) => {
-    const error = err.errors[0].validatorKey;
-    const msgError = require("../../../langue/errors.js")[langName].ORM[error];
-    
-    req.session.old = { username, password };
-    req.session.errors = { error, msgError };
+  /*************************************************************
+   *
+   * Validator check
+   *
+   *************************************************************/
+
+  req.session.errors = {};
+  req.session.old = { username, password };
+  const errorUsername = new validator(username, "username", { isAlphanumeric: true, min: 3, max: 20 }).validate(
+    req.session,
+    langName
+  );
+  const errorPassword = new validator(password, "password", { isAlphanumeric: true, min: 6, max: 40 }).validate(
+    req.session,
+    langName
+  );
+  if (errorUsername || errorPassword) {
     res.redirect(`/${langName}/register`);
-  });
+    return;
+  }
+
+  /*************************************************************
+   *
+   * Create user if don't exist
+   *
+   *************************************************************/
+
+  const [user, created] = await User.findOrCreate({ where: { username }, defaults: { username, password } });
+
+  if (created) {
+    res.redirect(`/${langName}/login`);
+    return;
+  }
+
+  const error = "alreadyExist";
+  const msgError = require("../../../langue/errors.js")[langName].ORM[error];
+  req.session.errors.username = [msgError];
+  console.log(req.session);
+  res.redirect(`/${langName}/register`);
 });
 
 module.exports = { get, post };
