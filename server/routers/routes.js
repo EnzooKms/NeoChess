90c7caeb8427821b00663458b759491d82973a98
Enzoo_Kms <ekemmas@edenschool.fr>
2024-02-19 13:57:00 +0100

    add auth middleware

--- server/routers/routes.js
+++ server/routers/routes.js
@@ -1,6 +1,6 @@
 const Router = require("./router.js");
 const router = new Router();
 
-router.route("home");
+router.route("home", ["auth"]);
 router.route("auth/register");
 router.route("auth/login");
