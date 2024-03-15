caee91da472b5850cce131e45e031d61bedbcd1b
Enzoo_Kms <ekemmas@edenschool.fr>
2024-03-16 00:15:09 +0100

    fix namespace redirect '/game/fr/register'

--- server/middlewares/middleware/auth.js
+++ server/middlewares/middleware/auth.js
@@ -13,7 +13,7 @@ const Middleware = require("../middleware.js");
 module.exports = new Middleware().middleware((req, res, next) => {
   const langName = req.params.language || config.default_language;
   if (!req.session.user) {
-    res.redirect(`${langName}/register`);
+    res.redirect(`/${langName}/register`);
     return;
   }
   next();
