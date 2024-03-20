1797b13d9687596a08309b46f2e0cdf4f0d406de
Enzoo_Kms <ekemmas@edenschool.fr>
2024-03-20 17:52:57 +0100

    add route chess/game

--- server/routers/routes.js
+++ server/routers/routes.js
@@ -4,3 +4,6 @@ const router = new Router();
 router.route("home", ["auth"]);
 router.route("auth/register");
 router.route("auth/login");
+
+router.route("chess/create", ["auth"]);
+router.route("chess/game", ["auth"]);
